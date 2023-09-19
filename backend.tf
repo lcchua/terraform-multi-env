50a6821d49b1460a4b427f7090f9a64b1c4f6779
iuolxvpq <luqmannurhakimt@gmail.com>
2023-09-20 07:02:39 +0800

    Update bucket region

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# Comment out the below if you are working on local environment
+# Comment out the below if you are working on local
 
 terraform {
   backend "s3" {
     bucket = "sctp-ce2-tfstate-bkt"
     key    = "luqman-tf-workspace-act.tfstate"   #Change the value of this to yourname-tf-workspace-act.tfstate for  example
-    region = "us-east-1"
+    region = "ap-southeast-1"
   }
 }
